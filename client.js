51f9c3bb7a608cbcff4962199ba0e992b04880e8
Weizhen Wang <wangwzhen@gmail.com>
2019-05-20 21:30:17 -0500

    completed

--- client.js
+++ client.js
@@ -4,25 +4,25 @@ let personToPick = '';
 function onReady() {
     promptName();
     //add all images to browser
-    addImage();
+    // addImage();
     //when click image, check if it's the right person
-    $('img').on('click', checkPerson);
+    $('.allPersons').on('click', 'img', checkPerson);
 }
 
 //function to add all images
-function addImage() {
-    people.forEach(function(person) {
-        let picture = $(`
-        <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
-        `)
-        $('.allPersons').append(picture);
-        picture.data('personName', person.name);
-        // console.log(picture.data());
-    })
+// function addImage() {
+//     people.forEach(function(person) {
+//         let picture = $(`
+//         <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+//         `)
+//         $('.allPersons').append(picture);
+//         picture.data('personName', person.name);
+//         // console.log(picture.data());
+//     })
     
-}
+// }
 
-//function to prompt a random name
+//function to prompt a random name and to add all images in random order
 function promptName() {
     //remove any border highlighting
     $('img').removeClass('visual');
@@ -30,7 +30,9 @@ function promptName() {
     // let sequenceNum = Math.floor(Math.random() * (1 + people.length - 1 - 0) + 0);
     personToPick = people[sequenceNum].name;
     $('#personName').text(personToPick);
-    //shuffle the image order
+    //clear out the existing images
+    //shuffle the image order and add new ones
+    $('.allPersons').empty();
     shuffleImg();
 }
 
@@ -58,10 +60,23 @@ function checkPerson() {
 
 //  function to shuffle the image order
 
-// function shuffleImg() {
-//     let tempArray = people;
-//     console.log(tempArray);
-//     for(let i = 0; i < people.length; i ++) {
-//         let index = Math.floor(Math.random() * (1 + tempArray.length - 1) + 1)
-//     }
-// }
+function shuffleImg() {
+    //create a tempArray to manipulate
+    let tempArray = people;
+    //loop through all the element in the array
+    for(let i = 0; i < people.length; i ++) {
+        //create random index
+        let index = Math.floor(Math.random() * tempArray.length);
+
+        //attach image at index to the browser
+        let picture = $(`
+        <img src="https://github.com/${tempArray[index].githubUsername}.png?size=250" alt="Profile image of ${tempArray[index].name}">
+        `)
+        $('.allPersons').append(picture);
+
+        //set the data method on the picture element
+        picture.data('personName', tempArray[index].name);
+        //remove the img at index from the tempArray
+        tempArray = tempArray.filter(person => tempArray.indexOf(person) !== index);
+    }
+}
