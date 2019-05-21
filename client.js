4941f0b28d7478bb187aff13acbb3ba33beb04d0
Weizhen Wang <wangwzhen@gmail.com>
2019-05-20 20:58:25 -0500

    hard mode completed

--- client.js
+++ client.js
@@ -24,22 +24,44 @@ function addImage() {
 
 //function to prompt a random name
 function promptName() {
-    let sequenceNum = Math.floor(Math.random() * people.length + 1);
-    // let sequenceNum = Math.floor(Math.random() * (1 + people.length - 1) + 1);
+    //remove any border highlighting
+    $('img').removeClass('visual');
+    let sequenceNum = Math.floor(Math.random() * people.length);
+    // let sequenceNum = Math.floor(Math.random() * (1 + people.length - 1 - 0) + 0);
     personToPick = people[sequenceNum].name;
     $('#personName').text(personToPick);
+    //shuffle the image order
+    shuffleImg();
 }
 
-// //function to check if it's the correct person clicked
+//function to check if it's the correct person clicked
 function checkPerson() {
+    //get the data method from the image
     let nameClicked = $(this).data().personName;
     console.log(nameClicked);
-    if(nameClicked === personToPick) {
-        alert('you got it!!!!!!! let\'s do another one!');
-        window.setTimeout(promptName, 2000);
 
+    //check if matched
+    if(nameClicked === personToPick) {
+
+        //if matched, show yellow border
+        $(this).addClass('visual');
+        //restart the game 2 sec after
+        setTimeout(function() {
+            promptName();
+            // alert('you got it!!!!!!! let\'s guess another one!');
+        }, 2000);
+        
     } else {
         alert('oops try again!');
     }
 }
 
+//  function to shuffle the image order
+
+// function shuffleImg() {
+//     let tempArray = people;
+//     console.log(tempArray);
+//     for(let i = 0; i < people.length; i ++) {
+//         let index = Math.floor(Math.random() * (1 + tempArray.length - 1) + 1)
+//     }
+// }
